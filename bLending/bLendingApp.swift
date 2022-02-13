330a8d6b040fa0485913102ce68da00c6b35458e
shyeon <sanghyeonlee137@gmail.com>
2022-02-13 15:37:29 +0900

    Initial Commit

---
+++ bLending/bLendingApp.swift
@@ -0,0 +1,17 @@
+//
+//  bLendingApp.swift
+//  bLending
+//
+//  Created by Sanghyeon Lee on 2022/02/13.
+//
+
+import SwiftUI
+
+@main
+struct bLendingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
